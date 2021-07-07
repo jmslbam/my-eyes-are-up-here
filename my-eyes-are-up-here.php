5f83958675ba9e9ccf2ea40e1b9fbdb685ab2ba8
Jaime Martínez <info@jaimemartinez.nl>
2021-07-07 17:52:28 +0200

    bump version and disable the plugin from updating itself

--- my-eyes-are-up-here.php
+++ my-eyes-are-up-here.php
@@ -1,9 +1,10 @@
 <?php
 /**
  * Plugin Name: My Eyes Are Up Here
- * Plugin URI: https://github.com/interconnectit/my-eyes-are-up-here
+ * Plugin URI: https://github.com/jmslbam/my-eyes-are-up-here
+ * Update URI: false
  * Description: Detects faces during thumbnail cropping and moves the crop position accordingly.
- * Version: 1.1.9
+ * Version: 1.1.11
  * Author: interconnect/it
  * Author URI: http://interconnectit.com
  *
